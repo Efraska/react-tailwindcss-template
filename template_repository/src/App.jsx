f8b05cfdf92f768e15e5c0313d44330baa489fc5
Efraska <aux_sistemas@medicainsurgentes.com.mx>
2024-12-31 11:21:41 -0600

    repositorio de plantilla de vite con tailwindcss y react

---
+++ template_repository/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1
+        className="text-3xl text-center text-red-700"
+      >Bienvenido a Vite con TailwindCSS y React</h1>
+    </>
+  )
+}
+
+export default App
